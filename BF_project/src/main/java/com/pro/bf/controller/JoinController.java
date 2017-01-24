a62ae94dd60556f06cc4239b49bd2f855a3cdf32
Cha Seung Hyun <chash512@nate.com>
2017-01-24 10:16:46 +0900

    commit 20170124

--- BF_project/src/main/java/com/pro/bf/controller/JoinController.java
+++ BF_project/src/main/java/com/pro/bf/controller/JoinController.java
@@ -1,5 +1,7 @@
 package com.pro.bf.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,8 +12,13 @@ public class JoinController {
 
 	@RequestMapping(value="joinForm", method=RequestMethod.GET)
 	public String JoinForm(){
-		String url = "joinForm";
+		String url = "join/joinForm";
 		return url;
 	}
 	
+	@RequestMapping(value="joinjoinForm", method=RequestMethod.GET)
+	public String JoinForm_Form(HttpServletRequest request){
+		String url = "join/joinjoinForm";
+		return url;
+	}
 }
