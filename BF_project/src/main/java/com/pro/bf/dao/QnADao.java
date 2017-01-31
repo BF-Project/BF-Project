076e7d81513b3b841a34dc54dd68219197579df7
KangJeongHyun <kimbab60@daum.net>
2017-01-31 13:36:00 +0900

    modify >> by kjh & kch

--- BF_project/src/main/java/com/pro/bf/dao/QnADao.java
+++ BF_project/src/main/java/com/pro/bf/dao/QnADao.java
@@ -1,5 +1,26 @@
 package com.pro.bf.dao;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import com.pro.bf.dto.QnAVO;
+
 public interface QnADao {
+
+	public ArrayList<QnAVO> listQna(String mbr_id) throws SQLException; // Q&A 리스트
+
+	public ArrayList<QnAVO> listAllQna(int tpage) throws SQLException; // Q&A ALL 리스트
+
+	public QnAVO getQna(int qna_num) throws SQLException;
+
+	public void insertQna(QnAVO qnaVO, String session_id) throws SQLException; // 등록
+
+	public int updateQna(QnAVO qnaVO) throws SQLException; // 수정
+	
+	public int countQna(QnAVO qnaVO) throws SQLException; // 조회수
+
+	public int totalQna() throws SQLException;
+
+	public void deleteQna(int qna_num)throws SQLException;//delete
 
 }
--- BF_project/src/main/java/com/pro/bf/serviceImpl/MbrServiceImpl.java
+++ BF_project/src/main/java/com/pro/bf/serviceImpl/MbrServiceImpl.java
@@ -1,24 +1,44 @@
 package com.pro.bf.serviceImpl;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import com.pro.bf.dao.MbrDao;
+import com.pro.bf.dao.QnADao;
 import com.pro.bf.daoImpl.MbrDaoImpl;
 import com.pro.bf.dto.MbrVO;
+import com.pro.bf.dto.QnAVO;
 import com.pro.bf.service.MbrService;
 
-public class MbrServiceImpl implements MbrService{
+@Service
+public class MbrServiceImpl implements MbrService {
 
 	@Autowired
 	private MbrDaoImpl mbrDao;
-	public void setMbrDAO(MbrDaoImpl mbrDao){
+
+	public void setMbrDAO(MbrDaoImpl mbrDao) {
 		this.mbrDao = mbrDao;
 	}
-	
-	
+
+	@Autowired
+	private QnADao qnaDAO;
+
+	public void setQnaDAO(QnADao qnaDAO) {
+		this.qnaDAO = qnaDAO;
+	}
+
+	@Autowired
+	private MbrDao mbrDAO;
+
+	public void setMbrDAO(MbrDao mbrDAO) {
+		this.mbrDAO = mbrDAO;
+	}
+
 	@Override
 	public void insertMember(MbrVO mbrVo) throws SQLException { // 회원가입
 		mbrDao.insertMember(mbrVo);
@@ -37,13 +57,12 @@ public class MbrServiceImpl implements MbrService{
 		return login;
 	}
 
-
 	@Override
-	public String searchUserName(String login) throws SQLException { // 로그인한 유저 이름 찾기
+	public String searchUserName(String login) throws SQLException { // 로그인한 유저
+																		// 이름 찾기
 		String loginUserName = mbrDao.searchUserName(login);
 		return loginUserName;
 	}
-
 
 	@Override
 	public String searchUserId(MbrVO mbrVo) throws SQLException { // 아이디 찾기
@@ -52,17 +71,51 @@ public class MbrServiceImpl implements MbrService{
 	}
 
 	@Override
-	public Map searchUserPwd(MbrVO mbrVo) throws SQLException { // 비밀번호 찾기 / 이메일도 같이 찾아야함
+	public Map searchUserPwd(MbrVO mbrVo) throws SQLException { // 비밀번호 찾기 /
+																// 이메일도 같이 찾아야함
 		MbrVO result = mbrDao.searchUserPwd(mbrVo);
 		Map<String, Object> searchMap = new HashMap<String, Object>();
-		try{
-			String pwd = result.getMbr_pwd();		
+		try {
+			String pwd = result.getMbr_pwd();
 			String email = result.getMbr_eml();
 			searchMap.put("pwd", pwd);
 			searchMap.put("email", email);
-		}catch(NullPointerException e){
+		} catch (NullPointerException e) {
 			searchMap.put("pwd", "not Information");
 		}
 		return searchMap;
 	}
+
+	@Override
+	public ArrayList<QnAVO> getQnaList(String mbr_id) throws SQLException {
+		ArrayList<QnAVO> qnaList = null;
+		try {
+			qnaList = qnaDAO.listQna(mbr_id);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return qnaList;
+	}
+
+	@Override
+	public QnAVO getQnaVO(int qna_num) throws SQLException {
+		QnAVO qnaVO = new QnAVO();
+		try {
+			qnaVO = qnaDAO.getQna(qna_num);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return qnaVO;
+	}
+
+	@Override
+	public void insertQna(QnAVO qnaVO, String mbr_id) throws SQLException {
+		try {
+			qnaDAO.insertQna(qnaVO, mbr_id);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+	}
+
 }
