cb9fa5b71b3c737711cba6c770b4490929b3e8ce
Cha Seung Hyun <chash512@nate.com>
2017-02-07 18:43:13 +0900

    chatting 60%

---
+++ BF_project/src/main/java/com/pro/bf/interceptor/HandshakeInterceptor.java
@@ -0,0 +1,31 @@
+package com.pro.bf.interceptor;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.http.server.ServletServerHttpRequest;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
+
+public class HandshakeInterceptor extends HttpSessionHandshakeInterceptor{
+
+	@Override
+	public boolean beforeHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler,
+			Map<String, Object> attributes) throws Exception {
+		System.out.println("beforeHandshake 이게 먼저 들어오고 chat");
+		// 파라메타에 attributes에 값을 저장하면 WebSocketSession에 전달된다.
+		ServletServerHttpRequest ssreq = (ServletServerHttpRequest) request;
+		HttpServletRequest req = ssreq.getServletRequest();
+		String loginUser = (String)req.getSession().getAttribute("loginUser");
+		attributes.put("ChatLogin", loginUser); // webSocketHandler session에서 값을 받아올 수 있도록
+		return super.beforeHandshake(request, response, wsHandler, attributes);
+	}
+
+	@Override
+	public void afterHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler, Exception ex) {
+		super.afterHandshake(request, response, wsHandler, ex);
+	}
+}
